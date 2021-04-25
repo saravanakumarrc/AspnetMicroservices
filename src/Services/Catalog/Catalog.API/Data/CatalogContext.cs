f6b5eec61dc754b70504e8fb2bc54449d3828580
Saravanakumar Radhakrishnan <saravanakumar_radhakrishnan@trimble.com>
2021-04-25 12:19:18 +0530

    Added catalog controller, repository

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Data.Interfaces;
+using Catalog.API.Entity;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+            var mongoClient = new MongoClient(connectionString);
+
+            var database = mongoClient.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:collectionName"));
+
+            CatalogContextSeed.Seed(Products);
+        }
+        public IMongoCollection<Product> Products { get; set; }
+    }
+}
