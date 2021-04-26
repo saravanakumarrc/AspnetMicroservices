7265be94fdaabce1504e37f1caae01e9a17ac811
Saravanakumar Radhakrishnan <saravanakumar_radhakrishnan@trimble.com>
2021-04-26 21:30:40 +0530

    moving the grpc project into correct location

--- src/Services/Discount/Discount.Grpc/Startup.cs
+++ src/Services/Discount/Discount.Grpc/Startup.cs
@@ -31,7 +31,7 @@ namespace Discount.Grpc
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                //endpoints.MapGrpcService<GreeterService>();
 
                 endpoints.MapGet("/", async context =>
                 {
